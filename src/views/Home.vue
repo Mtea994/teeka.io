5da260e2d158cd6654ab74aa0a5b8089443d7c94
mtea994 <20420633+Mtea994@users.noreply.github.com>
2021-10-10 21:03:27 +0500

    all markers done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,40 @@
        <l-marker :lat-lng="[34.0151, 71.5249]">
         <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
       </l-marker>
+     <l-marker :lat-lng="[33.9965, 71.4860]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       
+       <l-marker :lat-lng="[34.0119414, 71.5675742]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[33.9824482,  71.5627531]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[34.0127644, 71.5574755]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[34.0152562, 71.5800721]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[33.9203648, 71.5530499]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[34.1112497, 71.5596176]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[34.1320565, 71.4729769]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[ 34.0394998, 71.4655706]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[34.0134375, 71.5848125]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
+       <l-marker :lat-lng="[33.9909645, 71.4366741]">
+        <l-icon icon-url="https://image.flaticon.com/icons/png/512/101/101960.png" :icon-size="[24,24]" />
+      </l-marker>
       <l-control-layers />
     </l-map>
   </div>
